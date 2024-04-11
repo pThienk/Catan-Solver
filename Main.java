d2f96435352afdabff4b14e53f2748338006b310
Mark <1416050347@qq.com>
2024-04-11 17:24:34 -0400

    /* log - created the catan board - there are 19 hexes and 54 spots - the spots and hexes are labeled from 0 - n     - Both its ID and its location in list shows this information - each spot know of its adjacent hexes - each dpot know of its adjacent spots - each spot know of its port status - each hex know of its adjacent spot - types are managed by enums of Port_Type, Hex_Type     - Easy to be changed when interacting with user interface  */

--- Main.java
+++ Main.java
@@ -8,14 +8,14 @@ public class Main {
 
 /*
 log -
-created the catan board
+created the Catan board
 - there are 19 hexes and 54 spots
 - the spots and hexes are labeled from 0 - n
-    - Both its ID and its location in list shows this information
-- each spot know of its adjacent hexes
-- each spot know of its adjacent spots
-- each spot know of its port status
-- each hex know of its adjacent spot
-- types are managed by enums of Port_Type, Hex_Type
+    - Both their ID and their location in the list in <Board> shows this information
+- each spot knows of its adjacent hexes
+- each spot knows of its adjacent spots
+- each spot knows of its port status
+- each hex knows of its adjacent spots
+- types are managed by enums of Port_Type and Hex_Type
     - Easy to be changed when interacting with user interface
  */
